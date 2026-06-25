6e82dde083e2c6de84e4ef43b29b5d6fcfddb336
Erder <erderperez29@gmail.com>
2026-06-25 12:26:23 -0400

    Cambios

--- API_Usuario/Program.cs
+++ API_Usuario/Program.cs
@@ -88,7 +88,7 @@ builder.Services.AddAuthentication(
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     //app.MapOpenApi();
     app.UseSwagger();
